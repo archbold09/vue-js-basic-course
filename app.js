089c92316474a1a51d01018e482cfaaa979af45f
angel_archbold <angelarchbold09@gmail.com>
2020-10-28 06:55:51 -0500

    Vue - components of father to son

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 Vue.component("CoinDetail", {
-  props: ["changePercent", "title", "name", "img","currentPrice","pricesWithDays"],
+  props: ["coin"],
 
   data() {
     return {
       showPrices: false,
-      value: 800,
+      value: 0,
     };
   },
 
@@ -15,29 +15,32 @@ Vue.component("CoinDetail", {
   },
 
   computed: {
+    title() {
+      return `${this.coin.name} - ${this.coin.symbol}`;
+    },
     convertValue() {
       if (!this.value) {
         return 0;
       }
 
-      return this.value / this.currentPrice;
+      return this.value / this.coin.currentPrice;
     },
   },
 
   template: `
     <div>
-      <h1 :class="changePercent > 0 ? 'green' : 'red'">{{title}}</h1>
-      <img :src="img" :alt="name" />
+      <h1 :class="coin.changePercent > 0 ? 'green' : 'red'">{{title}}</h1>
+      <img :src="coin.img" :alt="coin.name" />
 
       <hr>
 
       <button v-on:click="toggleShowPrices">{{showPrices ? 'Hide prices' : 'Show prices'}}</button>
       
       <div v-if="showPrices">
-        <ul v-for="(price, index) in pricesWithDays" :key="price.day">
+        <ul v-for="(price, index) in coin.pricesWithDays" :key="price.day">
           <li
-          :class="{orange: price.value === currentPrice, green: price.value > currentPrice, red: price.value < currentPrice}"
-          >{{index}} {{price.day}} - {{price.value}}</li>
+          :class="{orange: price.value === coin.currentPrice, green: price.value > coin.currentPrice, red: price.value < coin.currentPrice}">
+          {{index}} {{price.day}} - {{price.value}}</li>
         </ul>
       </div>
 
@@ -54,49 +57,37 @@ new Vue({
 
   data() {
     return {
-      name: "Bitcoin",
-      symbol: "BTC",
-      img: "https://cryptologos.cc/logos/bitcoin-btc-logo.png",
-      changePercent: 12,
-      currentPrice: 8400,
-      symbol: "BTC",
-      pricesWithDays: [
-        { day: "Lunes", value: 8400 },
-        { day: "Martes", value: 7900 },
-        { day: "Miercoles", value: 8200 },
-        { day: "Jueves", value: 9000 },
-        { day: "Viernes", value: 9400 },
-        { day: "Sabado", value: 10000 },
-        { day: "Domingo", value: 10200 },
-      ],
-      showPrices: false,
+      btc: {
+        name: "Bitcoin",
+        symbol: "BTC",
+        img: "https://cryptologos.cc/logos/bitcoin-btc-logo.png",
+        changePercent: 12,
+        currentPrice: 8400,
+        symbol: "BTC",
+        pricesWithDays: [
+          { day: "Lunes", value: 8400 },
+          { day: "Martes", value: 7900 },
+          { day: "Miercoles", value: 8200 },
+          { day: "Jueves", value: 9000 },
+          { day: "Viernes", value: 9400 },
+          { day: "Sabado", value: 10000 },
+          { day: "Domingo", value: 10200 },
+        ],
+        name1: "",
+        symbol1: "",
+        value: 0,
+      },
       color: "f4f4f4",
-      name1: "",
-      symbol1: "",
-      value: 0,
     };
   },
-  computed: {
-    title() {
-      return `${this.name} - ${this.symbol}`;
-    },
-    title2() {
-      return `${this.name1} - ${this.symbol1}`;
-    },
-  },
-  watch: {
-    showPrices(newValue, oldValue) {
-      console.log(newValue, oldValue);
-    },
-  },
-  methods: {
-    toggleShowPrices() {
-      this.showPrices = !this.showPrices;
-      this.color = this.color.split("").reverse().join("");
-    },
-    handleChangeTitle() {
-      this.name1 = this.$refs.name1.value;
-      this.symbol1 = this.$refs.symbol1.value;
-    },
-  },
+  // methods: {
+  //   toggleShowPrices() {
+  //     this.showPrices = !this.showPrices;
+  //     this.color = this.color.split("").reverse().join("");
+  //   },
+  //   handleChangeTitle() {
+  //     this.name1 = this.$refs.name1.value;
+  //     this.symbol1 = this.$refs.symbol1.value;
+  //   },
+  // },
 });
